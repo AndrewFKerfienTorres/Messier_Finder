d4d3c9f69de4abfd84f65e95c604c58db5bbd833
Steve Lavoie <steve.a.lavoie@outlook.com>
2025-10-26 20:01:23 -0400

    Added messier objects loader

---
+++ src/main/java/MessierObjectLoader.java
@@ -0,0 +1,125 @@
+import CatalogueUtils.*;
+
+
+public class MessierObjectLoader {
+
+
+
+    public static void main(String []args){
+
+
+        CelestialObject m1 = new CelestialObject("M1",420,8.4,"Crab Nebula ","Taurus ","Super­nova remnant ","NGC 1952 ",4.9);
+        CelestialObject m2 = new CelestialObject( "M2",16,6.5,"— ","Aquarius ","Globular cluster ","NGC 7089 ",33);
+        CelestialObject m3 = new CelestialObject("M3",18,6.2,"— ","Canes Venatici ","Globular cluster ","NGC 5272 ",33.9);
+        CelestialObject m4 = new CelestialObject("M4",26,5.6,"Spider Globular Cluster ","Scorpius","Globular cluster ","NGC 6121 ",7.2);
+        CelestialObject m5 = new CelestialObject("M5",23,5.6,"Rose Cluster ","Serpens ","Globular cluster ","NGC 5904 ",24.5);
+        CelestialObject m6 = new CelestialObject("M6",25,4.2,"Butterfly Cluster ","Scorpius ","Open cluster ","NGC 6405 ",1.6);
+        CelestialObject m7 = new CelestialObject("M7",80,3.3,"Ptolemy's Cluster ","Scorpius ","Open cluster ","NGC 6475 ",0.65);
+        CelestialObject m8 = new CelestialObject( "M8",90,4.6,"Lagoon Nebula ","Sagittarius ","Nebula with cluster ","NGC 6523 ",4.1);
+        CelestialObject m9 = new CelestialObject("M9",9.3,7.7,"— ","Ophiuchus ","Globular cluster ","NGC 6333 ",25.8);
+        CelestialObject m10 = new CelestialObject("M10",20,6.6,"— ","Ophiuchus ","Globular cluster ","NGC 6254 ",14.3);
+        CelestialObject m11 = new CelestialObject( "M11",22.8,5.8,"Wild Duck Cluster ","Scutum ","Open cluster ","NGC 6705 ",6.2);
+        CelestialObject m12 = new CelestialObject( "M12",16,6.7,"— ","Ophiuchus ","Globular cluster ","NGC 6218 ",15.7);
+        CelestialObject m13 = new CelestialObject( "M13",20,5.8,"Great Hercules Cluster ","Hercules ","Globular cluster ","NGC 6205 ",22.2);
+        CelestialObject m14 = new CelestialObject(       "M14",11,7.6,"— ","Ophiuchus ","Globular cluster ","NGC 6402 ",30.30);
+        CelestialObject m15 = new CelestialObject(       "M15",18,6.2,"Great Pegasus Cluster ","Pegasus ","Globular cluster ","NGC 7078 ",330);
+        CelestialObject m16 = new CelestialObject(       "M16",70,6.4,"Eagle Nebula ","Serpens ","H II region nebula with cluster ","NGC 6611 ",70);
+        CelestialObject m17 = new CelestialObject(       "M17",11,6,"Omega, Swan, Horseshoe, Lobster, or Checkmark Nebula ","Sagittarius ","HII region nebula with cluster ","NGC 6618 ",50);
+        CelestialObject m18 = new CelestialObject(       "M18",9.8,7.5,"Black Swan Cluster ","Sagittarius ","Open cluster ","NGC 6613 ",4.90);
+        CelestialObject m19 = new CelestialObject(       "M19",17,6.8,"— ","Ophiuchus ","Globular cluster ","NGC 6273 ",28.70);
+        CelestialObject m20 = new CelestialObject(       "M20",28,6.3,"Trifid Nebula ","Sagittarius ","H II region nebula with cluster ","NGC 6514 ",5.20);
+        CelestialObject m21 = new CelestialObject(       "M21",14,6.5,"Webb's Cross Cluster ","Sagittarius ","Open cluster ","NGC 6531 ",4.250);
+        CelestialObject m22 = new CelestialObject(       "M22",32,5.1,"Great Sagittarius Cluster ","Sagittarius ","Globular cluster ","NGC 6656 ",9.60);
+        CelestialObject m23 = new CelestialObject(       "M23",35,5.5,"— ","Sagittarius ","Open cluster ","NGC 6494 ",2.150);
+        CelestialObject m24 = new CelestialObject(       "M24",2,2.5,"Small Sagittarius Star Cloud ","Sagittarius ","Milky Way star cloud ","IC 4715 ",100);
+        CelestialObject m25 = new CelestialObject(       "M25",36,4.6,"— ","Sagittarius ","Open cluster ","IC 4725 ",20);
+        CelestialObject m26 = new CelestialObject(       "M26",14,8,"— ","Scutum ","Open cluster ","NGC 6694 ",50);
+        CelestialObject m27 = new CelestialObject(       "M27",8,7.4,"Dumbbell Nebula ","Vulpecula ","Planetary nebula ","NGC 6853 ",1.1480);
+        CelestialObject m28 = new CelestialObject(       "M28",11.2,6.8,"— ","Sagittarius ","Globular cluster ","NGC 6626 ",17.90);
+        CelestialObject m29 = new CelestialObject(       "M29",7,7.1,"Cooling Tower Cluster ","Cygnus ","Open cluster ","NGC 6913 ",7.20);
+        CelestialObject m30 = new CelestialObject(       "M30",12,7.2,"Jellyfish Cluster ","Capricornus ","Globular cluster ","NGC 7099 ",27.80);
+        CelestialObject m31 = new CelestialObject(       "M31",3.17,3.4,"Andromeda Galaxy ","Andromeda ","Spiral galaxy ","NGC 224 ",24300);
+        CelestialObject m32 = new CelestialObject(       "M32",8.7,8.1,"Andromeda Satellite #1 ","Andromeda ","Dwarf elliptical galaxy ","NGC 221 ",24100);
+        CelestialObject m33 = new CelestialObject(       "M33",70.8,5.7,"Triangulum/Pinwheel Galaxy ","Triangulum ","Spiral galaxy ","NGC 598 ",23800);
+        CelestialObject m34 = new CelestialObject(       "M34",35,5.5,"Spiral Cluster ","Perseus ","Open cluster ","NGC 1039 ",1.50);
+        CelestialObject m35 = new CelestialObject(       "M35",28,5.3,"Shoe-Buckle Cluster ","Gemini ","Open cluster ","NGC 2168 ",2.80);
+        CelestialObject m36 = new CelestialObject(       "M36",12,6.3,"Pinwheel Cluster ","Auriga ","Open cluster ","NGC 1960 ",4.10);
+        CelestialObject m37 = new CelestialObject(       "M37",24,6.2,"Salt and Pepper Cluster ","Auriga ","Open cluster ","NGC 2099 ",4.5110);
+        CelestialObject m38 = new CelestialObject(       "M38",21,7.4,"Starfish Cluster ","Auriga ","Open cluster ","NGC 1912 ",4.20);
+        CelestialObject m39 = new CelestialObject(       "M39",29,4.6,"Pyramid Cluster ","Cygnus ","Open cluster ","NGC 7092 ",0.82440);
+        CelestialObject m40 = new CelestialObject(       "M40",51.7,8.4,"Winnecke 4 ","Ursa Major ","Optical Double ","— ",0.510);
+        CelestialObject m41 = new CelestialObject(       "M41",38,4.5,"Little Beehive Cluster ","Canis Major ","Open cluster ","NGC 2287 ",2.30);
+        CelestialObject m42 = new CelestialObject(       "M42",65,4,"Great Orion Nebula ","Orion ","H II region nebula ","NGC 1976 ",1.3240);
+        CelestialObject m43 = new CelestialObject(       "M43",20,9,"De Mairan's Nebula ","Orion ","H II region nebula (part of the Orion Nebula)","NGC 1982 ",1.60);
+        CelestialObject m44 = new CelestialObject(       "M44",95,3.7,"Beehive Cluster or Praesepe ","Cancer ","Open cluster ","NGC 2632 ",0.5770);
+        CelestialObject m45 = new CelestialObject(       "M45",2,1.6,"Pleiades, Seven Sisters or Subaru ","Taurus","Open cluster ","— ",0.390);
+        CelestialObject m46 = new CelestialObject(       "M46",22.8,6,"— ","Puppis ","Open cluster ","NGC 2437 ",5.40);
+        CelestialObject m47 = new CelestialObject(       "M47",30,4.4,"— ","Puppis ","Open cluster ","NGC 2422 ",1.60);
+        CelestialObject m48 = new CelestialObject(       "M48",30,5.5,"— ","Hydra ","Open cluster ","NGC 2548 ",1.50);
+        CelestialObject m49 = new CelestialObject(       "M49",10.2,8.4,"— ","Virgo ","Elliptical galaxy ","NGC 4472 ",536000);
+        CelestialObject m50 = new CelestialObject(       "M50",16,5.9,"Heart-Shaped Cluster ","Monoceros ","Open cluster ","NGC 2323 ",3.20);
+        CelestialObject m51 = new CelestialObject(       "M51",11.2,8.4,"Whirlpool Galaxy ","Canes Venatici ","Spiral galaxy ","NGC 5194,NGC 5195 ",190000);
+        CelestialObject m52 = new CelestialObject(       "M52",13,7.3,"Scorpion Cluster ","Cassiopeia ","Open cluster ","NGC 7654 ",50);
+        CelestialObject m53 = new CelestialObject(       "M53",13,7.6,"— ","Coma Berenices ","Globular cluster ","NGC 5024 ",580);
+        CelestialObject m54 = new CelestialObject(       "M54",12,7.6,"— ","Sagittarius ","Globular cluster ","NGC 6715 ",87.40);
+        CelestialObject m55 = new CelestialObject(       "M55",19,6.3,"Specter Cluster ","Sagittarius ","Globular cluster ","NGC 6809 ",17.60);
+        CelestialObject m56 = new CelestialObject(       "M56",8.8,8.3,"— ","Lyra ","Globular cluster ","NGC 6779 ",32.90);
+        CelestialObject m57 = new CelestialObject(       "M57",230,8.8,"Ring Nebula ","Lyra ","Planetary nebula ","NGC 6720 ",1.60);
+        CelestialObject m58 = new CelestialObject(       "M58",5.9,9.7,"— ","Virgo ","Barred Spiral galaxy ","NGC 4579 ",630000);
+        CelestialObject m59 = new CelestialObject(       "M59",5.4,9.6,"— ","Virgo ","Elliptical galaxy ","NGC 4621 ",550000);
+        CelestialObject m60 = new CelestialObject(       "M60",7.4,8.8,"— ","Virgo ","Elliptical galaxy ","NGC 4649 ",510000);
+        CelestialObject m61 = new CelestialObject(       "M61",6.5,9.7,"Swelling Spiral Galaxy ","Virgo","Spiral galaxy ","NGC 4303 ",502000);
+        CelestialObject m62 = new CelestialObject(       "M62",15,6.5,"Flickering Globular ","Ophiuchus ","Globular cluster ","NGC 6266 ",22.20);
+        CelestialObject m63 = new CelestialObject(       "M63",12.6,8.6,"Sunflower Galaxy ","Canes Venatici","Spiral galaxy ","NGC 5055 ",370000);
+        CelestialObject m64 = new CelestialObject(       "M64",10.7,8.5,"Black Eye Galaxy ","Coma Berenices ","Spiral galaxy ","NGC 4826 ",220000);
+        CelestialObject m65 = new CelestialObject(       "M65",8.7,9.3,"Leo Triplet ","Leo ","Barred Spiral galaxy ","NGC 3623 ",410000);
+        CelestialObject m66 = new CelestialObject(       "M66",9.1,8.9,"Leo Triplet ","Leo ","Barred Spiral galaxy ","NGC 3627 ",310000);
+        CelestialObject m67 = new CelestialObject(       "M67",30,6.1,"King Cobra or Golden Eye Cluster ","Cancer ","Open cluster ","NGC 2682 ",2.610);
+        CelestialObject m68 = new CelestialObject(       "M68",11,7.8,"— ","Hydra ","Globular cluster ","NGC 4590 ",33.60);
+        CelestialObject m69 = new CelestialObject(       "M69",10.8,7.6,"— ","Sagittarius ","Globular cluster ","NGC 6637 ",29.70);
+        CelestialObject m70 = new CelestialObject(       "M70",8,7.9,"— ","Sagittarius ","Globular cluster ","NGC 6681 ",29.40);
+        CelestialObject m71 = new CelestialObject(       "M71",7.2,8.2,"Angelfish Cluster ","Sagitta ","Globular cluster ","NGC 6838 ",130);
+        CelestialObject m72 = new CelestialObject(       "M72",6.6,9.3,"— ","Aquarius ","Globular cluster ","NGC 6981 ",53.40);
+        CelestialObject m73 = new CelestialObject(       "M73",2.8,9,"— ","Aquarius ","Asterism ","NGC 6994 ",2.50);
+        CelestialObject m74 = new CelestialObject(       "M74",10.5,9.4,"Phantom Galaxy[91] ","Pisces","Spiral galaxy ","NGC 628 ",240000);
+        CelestialObject m75 = new CelestialObject(       "M75",6.8,8.5,"— ","Sagittarius ","Globular cluster ","NGC 6864 ",67.50);
+        CelestialObject m76 = new CelestialObject(       "M76",2.7,10.1,"Little Dumbbell Nebula ","Perseus ","Planetary nebula ","NGC 650,NGC 651 ",2.50);
+        CelestialObject m77 = new CelestialObject(       "M77",7.1,8.9,"Cetus A or Squid Galaxy ","Cetus ","Spiral galaxy ","NGC 1068 ",470000);
+        CelestialObject m78 = new CelestialObject(       "M78",8,8.3,"— ","Orion ","Diffuse nebula ","NGC 2068 ",1.60);
+        CelestialObject m79 = new CelestialObject(       "M79",8.7,7.7,"— ","Lepus ","Globular cluster ","NGC 1904 ",410);
+        CelestialObject m80 = new CelestialObject(       "M80",10,7.3,"— ","Scorpius ","Globular cluster ","NGC 6093 ",32.60);
+        CelestialObject m81 = new CelestialObject(       "M81",26.9,6.9,"Bode's Galaxy ","Ursa Major","Spiral galaxy ","NGC 3031 ",114000);
+        CelestialObject m82 = new CelestialObject(       "M82",11.2,8.4,"Cigar Galaxy ","Ursa Major ","Starburst galaxy ","NGC 3034 ",107000);
+        CelestialObject m83 = new CelestialObject(       "M83",12.9,7.6,"Southern Pinwheel Galaxy ","Hydra ","Barred Spiral galaxy ","NGC 5236 ",147000);
+        CelestialObject m84 = new CelestialObject(       "M84",6.5,9.1,"— ","Virgo ","Lenticular galaxy ","NGC 4374 ",570000);
+        CelestialObject m85 = new CelestialObject(       "M85",7.1,9.1,"— ","Coma Berenices ","Lenticular galaxy ","NGC 4382 ",560000);
+        CelestialObject m86 = new CelestialObject(       "M86",8.9,8.9,"— ","Virgo ","Lenticular galaxy ","NGC 4406 ",490000);
+        CelestialObject m87 = new CelestialObject(       "M87",7.2,8.6,"Virgo A ","Virgo ","Elliptical galaxy ","NGC 4486 ",518700);
+        CelestialObject m88 = new CelestialObject(       "M88",6.9,9.6,"— ","Coma Berenices ","Spiral galaxy ","NGC 4501 ",390000);
+        CelestialObject m89 = new CelestialObject(       "M89",5.1,9.8,"— ","Virgo ","Elliptical galaxy ","NGC 4552 ",470000);
+        CelestialObject m90 = new CelestialObject(       "M90",9.5,9.5,"— ","Virgo ","Spiral galaxy ","NGC 4569 ",559000);
+        CelestialObject m91 = new CelestialObject(       "M91",5.4,10.2,"— ","Coma Berenices","Barred Spiral galaxy ","NGC 4548 ",470000);
+        CelestialObject m92 = new CelestialObject(       "M92",14,6.4,"— ","Hercules ","Globular cluster ","NGC 6341 ",26.70);
+        CelestialObject m93 = new CelestialObject(       "M93",10,6,"Critter Cluster ","Puppis ","Open cluster ","NGC 2447 ",3.60);
+        CelestialObject m94 = new CelestialObject(       "M94",11.2,8.2,"Crocodile Eye or Cat's Eye Galaxy ","Canes Venatici ","Spiral galaxy ","NGC 4736 ",147000);
+        CelestialObject m95 = new CelestialObject(       "M95",3.1,9.7,"— ","Leo ","Barred Spiral galaxy ","NGC 3351 ",312000);
+        CelestialObject m96 = new CelestialObject(       "M96",7.6,9.2,"— ","Leo ","Spiral galaxy ","NGC 3368 ",280000);
+        CelestialObject m97 = new CelestialObject(       "M97",3.4,9.9,"Owl Nebula ","Ursa Major ","Planetary nebula ","NGC 3587 ",2.030);
+        CelestialObject m98 = new CelestialObject(       "M98",9.8,10.1,"— ","Coma Berenices ","Spiral galaxy ","NGC 4192 ",444000);
+        CelestialObject m99 = new CelestialObject(       "M99",5.4,9.9,"St. Catherine's Wheel ","Coma Berenices","Spiral galaxy ","NGC 4254 ",447000);
+        CelestialObject m100 = new CelestialObject(       "M100",7.4,9.3,"Mirror Galaxy ","Coma Berenices ","Spiral galaxy ","NGC4321 ",550000);
+        CelestialObject m101 = new CelestialObject(       "M101",28.8,7.9,"Pinwheel Galaxy ","Ursa Major","Spiral galaxy ","NGC5457 ",191000);
+        CelestialObject m102 = new CelestialObject(       "M102",4.7,9.9,"Spindle Galaxy ","Draco ","Lenticular galaxy ","NGC5866 ",500000);
+        CelestialObject m103 = new CelestialObject(       "M103",6,7.4,"— ","Cassiopeia ","Open cluster ","NGC581 ",100);
+        CelestialObject m104 = new CelestialObject(       "M104",9,8,"Sombrero Galaxy ","Virgo ","Spiral galaxy ","NGC4594 ",287000);
+        CelestialObject m105 = new CelestialObject(       "M105",5.4,9.3,"— ","Leo ","Elliptical galaxy ","NGC3379 ",304000);
+        CelestialObject m106 = new CelestialObject(       "M106",18.6,8.4,"— ","Canes Venatici ","Spiral galaxy ","NGC4258 ",222000);
+        CelestialObject m107 = new CelestialObject(       "M107",10,7.9,"Crucifix Cluster ","Ophiuchus ","Globular cluster ","NGC6171 ",20.90);
+        CelestialObject m108 = new CelestialObject(       "M108",8.7,10,"Surfboard Galaxy ","Ursa Major ","Barred Spiral galaxy ","NGC3556 ",460000);
+        CelestialObject m109 = new CelestialObject(       "M109",7.6,9.8,"Vacuum Cleaner Galaxy ","Ursa Major","Barred Spiral galaxy ","NGC3992 ",595000);
+        CelestialObject m110 = new CelestialObject(       "M110",21.9,8.5,"Andromeda Satellite #2 ","Andromeda ","Dwarf elliptical galaxy ","NGC205 ",26000);
+
+        Catalogue c = Catalogue.getInstance();
+        //c.add(m1);
+    }
+}
