2518bdf728607ca6c39ad7e96d88190cb187e6d9
AndrewFKerfienTorres <akerfien@oswego.edu>
2025-11-04 18:05:16 -0500

    made Observatory serializable, made a constructor, and added getter functions

--- src/main/java/Observatory.java
+++ src/main/java/Observatory.java
@@ -1,7 +1,38 @@
+import java.io.Serializable;
 import java.util.TimeZone;
-public class Observatory {
-    double longitude = -76.550;
-    double latitude = 43.43; //initialized to rough estimates of the Rice Creek Telescope's location
-    String observatoryName = "Rice Creek Field Station";
-    TimeZone timeZone = TimeZone.getTimeZone("America/New_York");
+public class Observatory implements Serializable {
+    private double longitude;
+    private double latitude; //initialized to rough estimates of the Rice Creek Telescope's location
+    private String observatoryName;
+    private TimeZone timeZone;
+
+    public Observatory(){
+        longitude = -76.550;
+        latitude = 43.43;
+        observatoryName = "Rice Creek Field Station";
+        timeZone = TimeZone.getTimeZone("America/New_York");
+    }
+    public Observatory(double longitude, double latitude, String observatoryName, TimeZone t){
+        timeZone = t;
+        this.longitude = longitude;
+        this.latitude = latitude;
+        this.observatoryName = observatoryName;
+    }
+
+
+    public double getLongitude() {
+        return longitude;
+    }
+
+    public double getLatitude() {
+        return latitude;
+    }
+
+    public String getObservatoryName() {
+        return observatoryName;
+    }
+
+    public TimeZone getTimeZone() {
+        return timeZone;
+    }
 }
--- src/main/java/Telescope.java
+++ src/main/java/Telescope.java
@@ -1,4 +1,4 @@
-public class Telescope {
+public class Telescope implements Serializable{
 double fieldOfView = 0.0; //will need to be updated, I was unsure of the value for rice creek
 double mirrorDiameter = 16.0; //initialized for rice creek
 }
