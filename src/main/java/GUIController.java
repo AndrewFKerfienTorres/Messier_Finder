8dee0780e46b6bcac147c9379cbdba7375eb4ac4
dursokd <kdurso@oswego.edu>
2025-11-24 15:45:23 -0500

    GUI files

---
+++ src/main/java/GUIController.java
@@ -0,0 +1,29 @@
+import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class GUIController {
+    private Stage stage;
+    private Scene scene;
+    private Parent root;
+
+    public void switchHome(ActionEvent event) throws IOException {
+        Parent root = FXMLLoader.load(getClass().getResource("/fxml/home2.fxml"));
+        stage=(Stage)((Node)event.getSource()).getScene().getWindow();
+        scene= new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+    }
+    public void switchObject(ActionEvent event) throws IOException {
+        Parent root = FXMLLoader.load(getClass().getResource("/fxml/Main.fxml"));
+        stage=(Stage)((Node)event.getSource()).getScene().getWindow();
+        scene= new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+    }
+}
