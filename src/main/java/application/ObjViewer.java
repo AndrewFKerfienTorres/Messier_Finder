2f8df9fba5d19cf23385bd9d1326c5b6d2f23e88
evcapenos <evcapenos@gmail.com>
2025-12-02 11:21:12 -0500

    Update ObjViewer.java

--- src/main/java/application/ObjViewer.java
+++ src/main/java/application/ObjViewer.java
@@ -7,30 +7,39 @@ import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 public class Main extends Application {
+ 
+    private static CelestialObject selectedObject;
+
+    public static void setSelectedObject(CelestialObject obj) {
+        selectedObject = obj;
+    }
 
     @Override
     public void start(Stage primaryStage) {
         try {
-            FXMLLoader loader = new FXMLLoader(getClass().getResource("Main.fxml"));
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("/fxml/Main.fxml"));
             Parent root = loader.load();
+
+          
+            controller detailController = loader.getController();
+            if (selectedObject != null) {
+                detailController.displayCelestialObject(selectedObject);
+            }
 
             Scene scene = new Scene(root);
             primaryStage.setTitle("Messier Object Viewer");
             primaryStage.setScene(scene);
             primaryStage.show();
 
-            // gt controller so main GUI can interact with it
-            controller controller = loader.getController();
-
-            
-            // controller.setCommonName(idk);
-
         } catch (Exception e) {
             e.printStackTrace();
         }
     }
 
+}
+
     public static void main(String[] args) {
         launch(args);
     }
 }
+
