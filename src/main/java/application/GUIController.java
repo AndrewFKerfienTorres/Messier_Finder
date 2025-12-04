9af139c1bd874e713bf4305d7cc9cdb75bc6bda3
dursokd <kdurso@oswego.edu>
2025-12-04 10:17:20 -0500

    fixing guicontroller

--- src/main/java/application/GUIController.java
+++ src/main/java/application/GUIController.java
@@ -1,4 +1,33 @@
-@FXML private CheckBox filterNebula;
+package application;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.CheckBox;
+import javafx.scene.control.TextField;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.TilePane;
+import javafx.scene.layout.VBox;
+import javafx.scene.text.Text;
+import javafx.stage.Stage;
+import java.io.IOException;
+import java.time.Month;
+import java.time.ZoneOffset;
+import java.time.ZonedDateTime;
+import java.util.Optional;
+
+
+public class GUIController {
+    private Stage stage;
+    private Scene scene;
+    private Parent root;
+
+    @FXML private CheckBox filterNebula;
     @FXML private CheckBox filterGalaxy;
     @FXML private CheckBox filterOpenCluster;
     @FXML private CheckBox filterGlobularCluster;
