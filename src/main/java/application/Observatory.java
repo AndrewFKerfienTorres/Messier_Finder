c2d47cec3faa9b64a9bf34059f3addc65478ec55
ttanner04 <ttanner@oswego.edu>
2025-12-09 17:09:53 -0500

    Add telescope property and update constructors

--- src/main/java/application/Observatory.java
+++ src/main/java/application/Observatory.java
@@ -8,18 +8,30 @@ public class Observatory implements Serializable {
     private String observatoryName;
     private TimeZone timeZone;
     private Obstruction[] obstructions;
+    private Telescope telescope;
 
     public Observatory(){
         longitude = -76.550;
         latitude = 43.43;
         observatoryName = "Rice Creek Field Station";
         timeZone = TimeZone.getTimeZone("America/New_York");
+        obstructions = new Obstruction[]{
+                new Obstruction("tree", 0, 30, 0,40),
+                new Obstruction("tree",30, 90, 0,20),
+                new Obstruction("tree",90, 140, 0,40),
+                new Obstruction("tree",140, 240, 0,30),
+                new Obstruction("tree",240, 300, 0,60),
+                new Obstruction("tree",300, 360, 0,50)
+        };
+        telescope = new Telescope(90, 45);
+
     }
-    public Observatory(double longitude, double latitude, String observatoryName, TimeZone t){
+    public Observatory(double longitude, double latitude, String observatoryName, TimeZone t, Telescope telescope){
         timeZone = t;
         this.longitude = longitude;
         this.latitude = latitude;
         this.observatoryName = observatoryName;
+        this.telescope = telescope;
     }
 
 
@@ -56,5 +68,11 @@ public class Observatory implements Serializable {
         }
         newObstructions[i] = o;
         obstructions = newObstructions;
+    }
+    public Telescope getTelescope() {
+        return telescope;
+    }
+    public void setTelescope(Telescope t) {
+        this.telescope = t;
     }
 }
